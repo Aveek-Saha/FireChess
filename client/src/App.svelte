4bea8fb4b987319eca6f8f999da4e5236c7100e2
Aveek Saha <aveek.s98@gmail.com>
2021-04-24 13:44:46 +0530

    Add Google auth flow

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+    import Profile from './Profile.svelte';
+
+    import { auth, googleProvider } from './firebase';
+    import { authState } from 'rxfire/auth';
+
+    let user;
+
+    const unsubscribe = authState(auth).subscribe(u => user = u);
+
+    function login() {
+        auth.signInWithPopup(googleProvider);
+    }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<section>
+{#if user}
+    <Profile {...user} />
+    <button on:click={ () => auth.signOut() }>Logout</button>
+    <hr>
+{:else}
+	<button on:click={login}>
+		Sign in with Google
+	</button>
+{/if}
+</section>
---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyC6nb2_r-JTRSVs5-9celW69fKCn4fcjUw",
+    authDomain: "firechess0.firebaseapp.com",
+    databaseURL: "https://firechess0-default-rtdb.firebaseio.com",
+    projectId: "firechess0",
+    storageBucket: "firechess0.appspot.com",
+    messagingSenderId: "897411439363",
+    appId: "1:897411439363:web:bca1b67cd29e78e71e6736",
+    measurementId: "G-F7N1H13P3R"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export const db = firebase.firestore();
