ad5da35e54b29c3db420ee8628c03f9587ac45f3
Aveek Saha <aveek.s98@gmail.com>
2021-04-25 17:36:05 +0530

    Room creation test

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,15 +1,27 @@
 <script>
     import Profile from './Profile.svelte';
+    import Game from './Game.svelte';
 
-    import { auth, googleProvider } from './firebase';
+    import { auth, googleProvider, db } from './firebase';
+	import { collectionData } from 'rxfire/firestore';
     import { authState } from 'rxfire/auth';
 
     let user;
+	let roomID;
 
     const unsubscribe = authState(auth).subscribe(u => user = u);
 
     function login() {
         auth.signInWithPopup(googleProvider);
+    }
+
+	function createRoom() {
+        db.collection('rooms').add({ white: user.uid, gameBoard: "blank", gameStatus: "progress" });
+    }
+
+	function joinRoom(event) {
+        const { id, newStatus } = event.detail;
+        db.collection('todos').doc(id).update({ complete: newStatus });
     }
 </script>
 
@@ -19,6 +31,10 @@
     <Profile {...user} />
     <button on:click={ () => auth.signOut() }>Logout</button>
     <hr>
+	<button on:click={createRoom}>
+		Create Room
+	</button>
+	<Game uid={user.uid} />
 {:else}
 	<button on:click={login}>
 		Sign in with Google
---
+++ client/src/Game.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { db } from './firebase';
+    import { collectionData } from 'rxfire/firestore';
+    import { startWith } from 'rxjs/operators';
+
+    // User ID passed from parent
+    export let uid;
+
+    const query = db.collection('rooms').where('white', '==', uid)
+	let roomID = collectionData(query, 'id').pipe(startWith([]));;
+</script>
+
+<style>
+</style>
+
+
+<h3>Hi 
+    {#each $roomID as todo}
+
+    {todo.id}<br>
+    
+    {/each}
+</h3>
+<h3>Hi { uid }</h3>
