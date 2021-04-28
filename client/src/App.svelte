61af59e4b00cfec9a22e10b7c36bac2020366d26
Aveek Saha <aveek.s98@gmail.com>
2021-04-28 21:55:08 +0530

    Hide room options when in game

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,47 +2,15 @@
     import Profile from './Profile.svelte';
     import Room from './Room.svelte';
 
-    import { auth, googleProvider, db, arrayUnion } from './firebase';
-	import { collectionData } from 'rxfire/firestore';
+    import { auth, googleProvider } from './firebase';
     import { authState } from 'rxfire/auth';
 
     let user;
-	let roomID;
 
     const unsubscribe = authState(auth).subscribe(u => user = u);
 
     function login() {
         auth.signInWithPopup(googleProvider);
-    }
-
-	function createRoom() {
-        db.collection('rooms').add({
-            players: [user.uid], 
-            white: user.uid, 
-            gameBoard: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 
-            gameStatus: "progress" 
-        });
-    }
-
-	function joinRoom() {
-        var docRef = db.collection("rooms").doc(roomID);
-        docRef.get().then((doc) => {
-            if (doc.exists) {
-                console.log("Document data:", doc.data());
-                if(user.uid !== doc.data().white)
-                    docRef.update({ 
-                        black: user.uid,
-                        players: arrayUnion(user.uid)
-                    });
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
-        // db.collection('rooms').doc(roomID).update({ black: user.uid });
-        // roomID = ""
     }
 </script>
 
@@ -52,11 +20,6 @@
     <Profile {...user} />
     <button on:click={ () => auth.signOut() }>Logout</button>
     <hr>
-	<button on:click={createRoom}>
-		Create Room
-	</button>
-    <input bind:value={roomID}>
-    <button on:click={joinRoom}>Join Room</button>
 	<Room uid={user.uid} />
 {:else}
 	<button on:click={login}>
--- client/src/Room.svelte
+++ client/src/Room.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { db } from './firebase';
+    import { db, arrayUnion } from './firebase';
     import { collectionData } from 'rxfire/firestore';
     import { startWith } from 'rxjs/operators';
     import Game from './Game.svelte';
@@ -11,6 +11,8 @@
 	let rooms = collectionData(query, 'id').pipe(startWith([]));;
 
     let roomID = "";
+	let text;
+
 
     function selectRoom(id) {
         roomID = id
@@ -19,6 +21,34 @@
     function roomList() {
         roomID = "";
     }
+    
+	function createRoom() {
+        db.collection('rooms').add({
+            players: [uid], 
+            white: uid, 
+            gameBoard: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 
+            gameStatus: "progress" 
+        });
+    }
+
+	function joinRoom() {
+        var docRef = db.collection("rooms").doc(text);
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                if(uid !== doc.data().white)
+                    docRef.update({ 
+                        black: uid,
+                        players: arrayUnion(uid)
+                    });
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
+    }
 
     
 </script>
@@ -26,6 +56,13 @@
 <style>
 </style>
 
+{#if roomID==""}
+    <button on:click={createRoom}>
+        Create Room
+    </button>
+    <input bind:value={text}>
+    <button on:click={joinRoom}>Join Room</button>
+{/if}
 
 {#each $rooms as room}
     <br>
@@ -35,6 +72,7 @@
         </button>
     {/if}
     {#if roomID==room.id}
+        <br>
         <button on:click={roomList}>
             Back
         </button>
