552b3d2eb68494ffd909f1b0a7fc71c7be8211d9
Aveek Saha <aveek.s98@gmail.com>
2021-04-29 10:57:47 +0530

    Room list and button layout

--- client/src/Room.svelte
+++ client/src/Room.svelte
@@ -58,49 +58,64 @@
 </style>
 
 {#if roomID==""}
-<form>
-    <div class="row g-3">
-        <div class="col-auto">
-          <label for="inputText" class="visually-hidden">Room ID</label>
-          <input type="search" class="form-control" id="inputText" placeholder="Room ID" bind:value={text}>
-        </div>
-        <div class="col-auto">
-            <button type="button" class="btn btn-outline-info mb-3" on:click={joinRoom}>Join Room</button>
+<div class="row justify-content-around">
+    <div class="col-lg-4 col-md-5">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">Room List</h5>
+            </div>
+            <div class="list-group list-group-flush">
+                {#each $rooms as room}
+                            <button type="button" class="list-group-item list-group-item-action" 
+                            on:click={selectRoom(room.id)}>
+                            {room.id}
+                        </button>
+                {/each}
+            </div>
         </div>
     </div>
-    <div class="row mb-3">
-        <div class="col-auto">
-            <button type="button" class="btn btn-outline-success" on:click={createRoom}>
-                Create Room
-            </button>
-        </div>
+    <div class="col-auto">
+        <form>
+            <div class="row g-3">
+                <div class="col-auto">
+                    <label for="inputText" class="visually-hidden">Room ID</label>
+                    <input type="search" class="form-control" id="inputText" placeholder="Room ID" bind:value={text}>
+                </div>
+                <div class="col-auto">
+                    <button type="button" class="btn btn-outline-info mb-3" on:click={joinRoom}>Join Room</button>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-auto">
+                    <button type="button" class="btn btn-outline-success" on:click={createRoom}>
+                        Create Room
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
-  </form>
-{/if}
+</div>
 
 
-{#if roomID!=""}
-<form class="row g-3 mb-3">
-    <div class="col-12">
-        <button type="button" class="btn btn-primary" on:click={roomList}>
-            Back
-        </button>
-    </div>
-</form>
 {/if}
 
 <div class="row">
-    <div class="col-4">
+    <div class="col-12">
+        {#if roomID!=""}
+        <form class="row g-3 mb-3">
+            <div class="col-12">
+                <button type="button" class="btn btn-primary" on:click={roomList}>
+                    Back
+                </button>
+            </div>
+        </form>
+        {/if}
         <div class="list-group">
             {#each $rooms as room}
-                {#if roomID==""}
-                        <button type="button" class="list-group-item list-group-item-action" 
-                        on:click={selectRoom(room.id)}>
-                        {room.id}
-                    </button>
-                {/if}
                 {#if roomID==room.id}
-                    <Game id={room.id} uid={uid}/>
+                    <div class="col-md-6 offset-md-3">
+                        <Game id={room.id} uid={uid}/>
+                    </div>
                 {/if}
             {/each}
         </div>
